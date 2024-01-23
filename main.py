4a531ceb3c0205a163f74753bb66dca9bdb471c9
ragamafia <pragim.atat@mail.ru>
2024-01-23 17:42:05 +0800

    test

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-if __name__ == '__main__':
-    pass
+import config
+
+from aiogram import Bot, Dispatcher, executor, types
+
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
+
+
 
