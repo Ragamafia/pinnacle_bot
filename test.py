b502f66226e72eee44aa83e2fc6b0f16ff075a44
ragamafia <pragim.atat@mail.ru>
2024-01-23 22:41:31 +0800

    update aiogram (3.3.0)

---
+++ test.py
@@ -0,0 +1,21 @@
+import asyncio
+import time
+
+
+async  def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+async def main():
+
+    task_1 = asyncio.create_task(say_after(1, 'hello'))
+    task_2 = asyncio.create_task(say_after(3, 'world!'))
+
+    print(f'started at {time.strftime("%X")}')
+
+    await task_1
+    await task_2
+
+    print(f'finished at {time.strftime("%X")}')
+
+asyncio.run(main())
